61c436b38351cbafefc7612c8bccb612e42ac747
Liangjiong Zhou <liangjiong.zhou@smartsenstech.com>
2026-01-30 14:19:09 +0800

    Update build script
    Skip sdk build if it already has been successflly comiled.

--- smartsens_sdk/scripts/a1_sc132gs_build.sh
+++ smartsens_sdk/scripts/a1_sc132gs_build.sh
@@ -6,20 +6,14 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]-$0}")" && pwd)
 ROOT_DIR=$(cd "${SCRIPT_DIR}/.." && pwd)
 CACHE_DIR=${ROOT_DIR}/cache
 SERVER_OUTPUT_DIR=/home/a1_output
+ZIMAGE=${ROOT_DIR}/output/build/linux-custom/arch/arm/boot/zImage.smartsens-m1-evb
 
-## 修改 app_config.json 中的图片尺寸参数
-#CONFIG_FILE="${ROOT_DIR}/smart_software/src/app_demo/ped_detection/ssne_ai_demo/app_assets/app_config.json"
-#if [ -f "$CONFIG_FILE" ]; then
-#    # 使用 sed 命令修改 JSON 文件中的 img_height 和 img_width
-#    sed -i 's/"img_height": [0-9]\+/"img_height": 1280/g' "$CONFIG_FILE"
-#    sed -i 's/"img_width": [0-9]\+/"img_width": 720/g' "$CONFIG_FILE"
-#    echo "已更新 app_config.json 中的图片尺寸参数"
-#else
-#    echo "警告: 配置文件 $CONFIG_FILE 不存在"
-#fi
-
-
-bash ${SCRIPT_DIR}/build_release_sdk.sh
+if [[ -f ${ZIMAGE} ]]; then
+    echo "The SDK has been successfully compiled, skip first build_release_sdk.sh"
+else
+    echo "The SDK has not been successfully compiled, do build_release_sdk.sh first"
+    bash ${SCRIPT_DIR}/build_release_sdk.sh
+fi
 bash ${SCRIPT_DIR}/build_app.sh
 bash ${SCRIPT_DIR}/build_release_sdk.sh
 
